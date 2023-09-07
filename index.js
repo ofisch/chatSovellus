c8a9cb22b76a139658ca1959da83e68f933d7452
ofisch <onni.fischer@gmail.com>
2023-09-07 17:36:10 +0300

    käyttäjänimet toimii

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
 
-  socket.on('disconnect', () => {
-    console.log('a user disconnected', socket.id);
+  socket.on("send-nickname", (nickname) => {
+    socket.nickname = nickname;
+    users.push(socket.nickname);
+    console.log("index.js nick: ", nickname);
+    console.log("users: ", users);
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ', msg);
-    io.emit('chat message', msg);
+  socket.on("disconnect", () => {
+    console.log("a user disconnected", socket.id);
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: ", msg);
+    io.emit("chat message", msg);
   });
 });
 
 http.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log("listening on port 3000");
 });
--- public/chat.js
+++ public/chat.js
@@ -3,13 +3,34 @@
 // Server URL below must point to your server, localhost works for local development/testing
 const socket = io("http://localhost:3000");
 
-document.querySelector("form").addEventListener("submit", (event) => {
+const messageBox = document.getElementById("m");
+messageBox.hidden = true;
+const sendButton = document.getElementById("send");
+sendButton.hidden = true;
+
+const enterNicknameForm = document.getElementById("enter-nickname");
+
+document
+  .getElementById("enter-nickname")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nicknameInput = document.getElementById("nickname");
+    console.log("nick: ", nicknameInput.value);
+    socket.emit("send-nickname", nicknameInput.value);
+    //socket.emit("sendNickname", nickname);
+
+    enterNicknameForm.hidden = true;
+
+    messageBox.hidden = false;
+    sendButton.hidden = false;
+  });
+
+document.getElementById("send-message").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const nickname = document.getElementById("nickname").value;
-
   const inp = document.getElementById("m");
-  socket.emit("chat message", nickname + ":" + inp.value);
+  socket.emit("chat message", nickname.value + ":" + inp.value);
   inp.value = "";
 });
 
